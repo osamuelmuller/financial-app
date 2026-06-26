7a185ea28ef5bec2552aa64dc051eded53def653
Samuel Muller <samuelmuller.login@gmail.com>
2026-06-26 10:41:45 -0300

    spring boot setup completed - PostgreSQL connection started

---
+++ finances-api/src/main/java/com/samuel/finances_api/FinancesApiApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.finances_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApiApplication.class, args);
+	}
+
+}
---
+++ finances-api/src/test/java/com/samuel/finances_api/FinancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.finances_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
