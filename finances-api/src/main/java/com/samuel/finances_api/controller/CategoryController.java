55d3ea0088dcbfe419d2196110a23b271fed8409
Samuel Muller <samuelmuller.login@gmail.com>
2026-07-02 10:02:39 -0300

    created CategoryController with CRUD endpoints

---
+++ finances-api/src/main/java/com/samuel/finances_api/controller/CategoryController.java
@@ -0,0 +1,43 @@
+package com.samuel.finances_api.controller;
+
+import com.samuel.finances_api.dto.category.CategoryResponse;
+import com.samuel.finances_api.dto.category.CreateCategoryRequest;
+import com.samuel.finances_api.dto.category.UpdateCategoryRequest;
+import com.samuel.finances_api.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryResponse> getAll() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse getById(@PathVariable Long id) {
+        return categoryService.getById(id);
+    }
+
+    @PostMapping
+    public CategoryResponse create(@RequestBody CreateCategoryRequest request) {
+        return categoryService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponse update(@PathVariable Long id, @RequestBody UpdateCategoryRequest request) {
+        return categoryService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+}
